ae6671d161dc446d74bfaad8fd61b1d34154cc37
Olivier Blanc <olivier.blanc@gmail.com>
2016-06-10 19:08:41 +0200

    Tests

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function counter(state, action) {
-    var current = state || 0;
+const counter = (state = 0, action) => {
     switch (action.type) {
         case "INCREMENT":
             return state + 1;
+        case "DECREMENT":
+            return state - 1;
         default:
             return state;
     }
-}
+};
+
+module.exports = counter;
---
+++ index.test.js
@@ -0,0 +1,22 @@
+const expect = require('expect');
+const counter = require('./index.js');
+
+expect(counter(undefined, {}))
+    .toEqual(0);
+
+expect(counter(4, {type: 'UNKNOWN'}))
+    .toEqual(4);
+
+expect(counter(0, {type:'INCREMENT'}))
+    .toEqual(1);
+
+expect(counter(2, {type:'INCREMENT'}))
+    .toEqual(3);
+
+expect(counter(1, {type:'DECREMENT'}))
+    .toEqual(0);
+
+expect(counter(4, {type:'DECREMENT'}))
+    .toEqual(3);
+
+console.log("All tests passed!");
