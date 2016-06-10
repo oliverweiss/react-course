3b72ccf50130a1a994c08a9105e7e42ee0ecbe8f
Olivier Blanc <olivier.blanc@gmail.com>
2016-06-10 23:10:27 +0200

    home made store

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const {createStore} = require('redux');
+// const {createStore} = require('redux');
 const counter = require('./counter.js');
+
+const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => listeners = listeners.filter(l => l != listener);
+    };
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(l => l());
+    }
+
+    dispatch({});
+
+    return {getState, subscribe, dispatch};
+};
 
 const store = createStore(counter);
 
 const render = () => document.body.innerText = store.getState();
 
 store.subscribe(render);
-document.addEventListener("click", () => store.dispatch({type:'INCREMENT'}));
 render();
+
+document.addEventListener("click", () => store.dispatch({type:'INCREMENT'}));
