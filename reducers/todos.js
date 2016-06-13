58c9002d4c34c0004cad063a93dc8cc3842648b4
Olivier Blanc <olivier.blanc@gmail.com>
2016-06-13 09:59:35 +0200

    Full todos reducer

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,9 +1,30 @@
+const addTodo = (state, text) => [{text, done:false}, ...state];
+const toggle = (todo) => ({...todo, done: !todo.done});
+const toggleTodo = (state, position) => state.map((todo, index) => position === index ? toggle(todo) : todo);
+const editTodo = (state, position, text) => state.map((todo, index) => position === index ? {...todo, text} : todo);
+const ORIGIN = 0;
+const clearTodo = (state, position) => [...state.slice(ORIGIN, position), ...state.slice(position + 1)];
+const toggleAllTodos = (state) => {
+    const done = state.some((todo) => !todo.done);
+
+    return state.map((todo) => ({...todo, done}));
+};
+const clearCompletedTodos = (state) => state.filter((todo) => !todo.done);
+
 const todos =  (state = [], action) => {
     switch(action.type){
         case 'ADD_TODO':
-            return [...state, {text: action.text, done:false}];
+            return addTodo(state, action.text);
+        case 'EDIT_TODO':
+            return editTodo(state, action.position, action.text);
+        case 'CLEAR_TODO':
+            return clearTodo(state, action.position);
         case 'TOGGLE_TODO':
-            return state.map((todo, index) => action.position === index ? {...todo, done: !todo.done} : todo);
+            return toggleTodo(state, action.position);
+        case 'TOGGLE_ALL_TODOS':
+            return toggleAllTodos(state);
+        case 'CLEAR_COMPLETED_TODOS':
+            return clearCompletedTodos(state);
         default:
             return state;
     }
--- test/reducers/todos.test.js
+++ test/reducers/todos.test.js
@@ -1,18 +1,68 @@
+/* eslint-env node, mocha */
 const expect = require('expect');
 const todos = require('../../reducers/todos.js');
 
-describe('List reducer', function() {
-    it('should return an empty list when initialized', function () {
-        expect(todos(undefined, {})).toEqual([]);
+describe('Todos reducer', () => {
+    it('should return an empty list when initialized',  () => {
+        expect(todos(undefined, {})).toEqual([]); // eslint-disable-line no-undefined
     });
-    it('should ignore unknown actions', function () {
-        expect(todos([{},{},{}], {type:'UNKNOWN'})).toEqual([{},{},{}]);
+    it('should ignore unknown actions',  () => {
+        expect(todos([{}, {}, {}], {type:'UNKNOWN'})).toEqual([{}, {}, {}]);
     });
-    it('should append a todo when ADD_TODO', function () {
+    it('should add a todo when ADD_TODO',  () => {
         expect(todos([], {type:'ADD_TODO', text:'Learn Redux'})).toEqual([{text:'Learn Redux', done:false}]);
     });
-    it('should toggle a todo when TOGGLE_TODO', function () {
-        expect(todos([{text:'Learn Redux', done:false}], {type:'TOGGLE_TODO', position:0})).toEqual([{text:'Learn Redux', done:true}]);
+    const todoA = {text:'Learn Redux', done:false};
+    const todoB = {text:'Have fun', done:true};
+
+    it('should prepend a todo when ADD_TODO',  () => {        
+        expect(todos([todoA, todoB], {type:'ADD_TODO', text:'Take over the world'})).toEqual([{text:'Take over the world', done:false}, todoA, todoB]);
+    });    
+    it('should edit a todo when EDIT_TODO',  () => {        
+        expect(todos(
+            [todoA, {text:'Learn Redux', done:false}],
+            {type:'EDIT_TODO', position:1, text: 'Learn React'})
+        ).toEqual(
+            [todoA, {text:'Learn React', done:false}]);
+    });
+    it('should delete a todo when CLEAR_TODO',  () => {        
+        expect(todos(
+            [todoA, {text:'Learn Redux', done:false}],
+            {type:'CLEAR_TODO', position:1})
+        ).toEqual(
+            [todoA]);
+    });
+    it('should toggle a todo when TOGGLE_TODO', () => {
+        expect(todos([todoA, {text:'Learn Redux', done:false}], {type:'TOGGLE_TODO', position:1})).toEqual([todoA, {text:'Learn Redux', done:true}]);
+    });
+    it('should complete all todos when TOGGLE_ALL_TODOS and at least one is undone', () => {
+        expect(todos([todoB, {text:'Learn Redux', done:false}], {type:'TOGGLE_ALL_TODOS'})).toEqual([todoB, {text:'Learn Redux', done:true}]);
+    });
+    it('should uncomplete all todos when TOGGLE_ALL_TODOS and all complete', () => {
+        expect(todos(
+            [
+                {text: 'Have fun', done:true},
+                {text:'Learn Redux', done:true}
+            ],
+            {type:'TOGGLE_ALL_TODOS'})
+        ).toEqual(
+            [
+                {text: 'Have fun', done:false},
+                {text:'Learn Redux', done:false}
+            ]);
+    });
+    it('should clear all completed todos when CLEAR_COMPLETED_TODOS', () => {
+        expect(todos(
+            [
+                {text: 'Have fun', done:true},
+                {text:'Learn Redux', done:true},
+                {text:'Take over the world', done:false}
+            ],
+            {type:'CLEAR_COMPLETED_TODOS'})
+        ).toEqual(
+            [
+                {text:'Take over the world', done:false}
+            ]);
     });
 
 });
