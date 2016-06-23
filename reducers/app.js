66630d88b1188a50aa72e4c0decaea933bb4dbfb
Olivier Blanc <olivier.blanc@gmail.com>
2016-06-23 22:13:38 +0200

    Homemade combine reducers

--- reducers/app.js
+++ reducers/app.js
@@ -1,9 +1,19 @@
 const todos = require('./todos.js');
 const filter = require('./filter.js');
 
-const app = (state = {}, action) => ({
-    todos : todos(state.todos, action),
-    filter : filter(state.filter, action)
-});
+const combineReducers = (reducers) =>
+    (state = {}, action) => 
+        Object.keys(reducers).reduce((_reducer, key) =>
+            ({
+                ..._reducer,
+                [key]: reducers[key](state[key], action)
+            }), {});
+
+const app = combineReducers({todos, filter});
+
+// const app = (state = {}, action) => ({
+//     todos : todos(state.todos, action),
+//     filter : filter(state.filter, action)
+// });
 
 module.exports = app;
